0248a8e8bc628b3d4c9ad34575bd128332158fd0
DeadJester <DeadJester@users.noreply.github.com>
2017-11-01 10:55:08 -0700

    major update
    side bar update
    finished contact us page
    added links
    cleaned up header
    removed service page and made that a link in the side bar as a document
    shuffled more files for ease of access and maintainance

--- Web-Page/JS/Scroll-Button.js
+++ Web-Page/JS/Scroll-Button.js
@@ -7,10 +7,10 @@ function scrollFunction() {
     } else {
         document.getElementById("myBtn").style.display = "none";
     }
-}
+};
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0; // For Chrome, Safari and Opera 
     document.documentElement.scrollTop = 0; // For IE and Firefox
-}
+};
